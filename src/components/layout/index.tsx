e093cbc264300bfe81bb6e02fe34dd9959e03011
Sebastian Campo <campo.sebastian44@gmail.com>
2025-02-03 00:08:49 -0300

    layout

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Hamburger } from "../../ui/buttons";
-import * as css from "./style.css"
+import * as css from "./style.css"; 
+import { SidebarMenu } from "../sidebar";
 const brandLogo = require("../../lib/brand-logo.svg");
 
 export function Layout(){
+    const [menuState, setMenuState] = useState(false)
+
 
     function handleClickLogo(){
-        
+        console.log("click home")
+    }
+
+    function handleHamburgerClick(){
+        if(menuState){
+            setMenuState(false)
+        }
+        if(!menuState){
+            setMenuState(true)
+        }
     }
 
     return (
         <div>
             <div className={css.navbar}>
                 <div className={css.brand} onClick={handleClickLogo} dangerouslySetInnerHTML={{ __html: brandLogo }} />
-                <Hamburger/>
+                <div onClick={handleHamburgerClick}>
+                    <Hamburger/>
+                </div>
+                {/* Aca hay que ver si el div del sidebar se puede ocultar mientras esté off, ocupa un espacio innecesario */}
+                <div>
+                    <SidebarMenu disabled={menuState}/>
+                </div>
             </div>
         </div>
     )
