2b6941456fa394e8b9d834321ae1ca6b75f35580
Sebastian <campo.sebastian44@gmail.com>
2025-03-19 17:32:11 -0300

    saved dom

---
+++ src/pages/Mascotas1/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useUserPosition } from "./hookss";
+import { Card } from "../../components/pet-card";
+import { Suspense } from "react";
+import { Loading } from "../../components/fallback-loading";
+import { ReportForm } from "../../components/report-form";
+import * as css from "./style.css"
+
+export function Mascotas(){
+    const pets = useUserPosition();
+    
+
+    return (
+        <div className={css.root}>   
+            <div className={css.heroDiv}>
+                <p>Mascotas perdidas cerca</p>
+            </div>
+
+            <Suspense fallback={<Loading />}>
+                {pets.map((p: any)=>{
+                    return <Card 
+                        key={Math.random()}
+                        id={p.id}
+                        nombre={p.nombre}
+                        imagen={p.imagen}
+                        found={p.found}
+                        locationName={p.locationName}
+                    />
+                })}
+            </Suspense>
+            <div className={css.reportForm}>
+                <ReportForm />   
+            </div>
+
+        </div>
+    )
+}
